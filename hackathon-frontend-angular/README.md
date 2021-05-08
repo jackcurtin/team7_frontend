b67b8775c25d0d54be4db0b12541da8443286223
Jack Curtin <jackcurtin94@gmail.com>
2021-05-08 09:26:10 -0500

    angular app recreated

--- hackathon-frontend-angular/README.md
+++ hackathon-frontend-angular/README.md
@@ -1,4 +1,4 @@
-# HackathonFrontend
+# HackathonFrontendAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.10.
 
--- hackathon-frontend-angular/src/app/app.component.spec.ts
+++ hackathon-frontend-angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hackathon-frontend'`, () => {
+  it(`should have as title 'hackathon-frontend-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hackathon-frontend');
+    expect(app.title).toEqual('hackathon-frontend-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hackathon-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('hackathon-frontend-angular app is running!');
   });
 });
