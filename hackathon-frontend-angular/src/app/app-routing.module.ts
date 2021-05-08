0f09941008b315cd2dbdd4cb9300f515a244042c
Jack Curtin <jackcurtin94@gmail.com>
2021-05-08 09:45:46 -0500

    created form component

--- hackathon-frontend-angular/src/app/app-routing.module.ts
+++ hackathon-frontend-angular/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormComponent} from './form/form.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {
+      path: 'form',
+      component: FormComponent
+    }
+    ])
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
